5d8ac8896ed817775f8979af1de264df314a82fb
Imran221b <imran221b@gmail.com>
2018-07-08 22:19:04 +0600

    Update Segment Tree with lazy.cpp

--- Segment Tree with lazy.cpp
+++ Segment Tree with lazy.cpp
@@ -1,91 +1,102 @@
-//From Shafaets planet
- 
-#define mx 100001
-int arr[mx];
-int tree[mx * 3];
-void init(int node, int b, int e)
-{
+//Array and Segment tree is indexed from 1
+//Works for getting the sum. Change according to problem. 
+
+vlong ar[ARRAY_SIZE], tr[ARRAY_SIZE * 4];
+
+void init(int node, int b, int e) {
     if (b == e) {
-        tree[node] = arr[b];
+        tr[node] = ar[b];
         return;
     }
-    int Left = node * 2;
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    init(Left, b, mid);
-    init(Right, mid + 1, e);
-    tree[node] = tree[Left] + tree[Right];
+
+    int lf = node << 1, ri = lf | 1, md = b+e >> 1;
+    
+    init(lf, b, md);
+    init(ri, md+1, e);
+    
+    tr[node] = tr[lf] + tr[ri];
 }
  
-int query(int node, int b, int e, int i, int j)
-{
-    if (i > e || j < b)
+vlong query(int node, int b, int e, int i, int j) {
+
+    if (i > e || j < b) {
         return 0;
-    if (b >= i && e <= j)
-        return tree[node];
-    int Left = node * 2;
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    int p1 = query(Left, b, mid, i, j);
-    int p2 = query(Right, mid + 1, e, i, j);
-    return p1 + p2;
+    }
+
+    if (b >= i && e <= j) {
+        return tr[node];
+    }
+    
+    int lf = node << 1, ri = lf | 1, md = b+e >> 1;
+
+    vlong x = query(lf, b, md, i, j);
+    vlong y = query(ri, md+1, e, i, j);
+
+    return x+y;
 }
-void update(int node, int b, int e, int i, int newvalue)
-{
-    if (i > e || i < b)
-        return;
-    if (b >= i && e <= i) {
-        tree[node] = newvalue;
+
+
+void update(int node, int b, int e, int i, vlong newvalue) {
+    if (i > e || i < b) {
         return;
     }
-    int Left = node * 2;
-    int Right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    update(Left, b, mid, i, newvalue);
-    update(Right, mid + 1, e, i, newvalue);
-    tree[node] = tree[Left] + tree[Right];
+
+    if (b >= i && e <= i) {
+        tr[node] = newvalue;
+        return;
+    }
+
+    int lf = node << 1, ri = lf | 1, md = b+e >> 1;
+
+    update(lf, b, md, i, newvalue);
+    update(ri, md+1, e, i, newvalue);
+
+    tr[node] = tr[lf] + tr[ri];
 }
  
  
 //LAZY
+//TODO: Change the update function so it works for non-associative functions as well
  
 struct info {
-    i64 prop, sum;
-} tree[mx * 3];
-void update(int node, int b, int e, int i, int j, i64 x)
-{
-    if (i > e || j < b)
-        return;
-    if (b >= i && e <= j)
-    {
-        tree[node].sum += ((e - b + 1) * x);
-        tree[node].prop += x;
+    vlong prop, sum;
+} tr[ARRAY_SIZE * 4];
+
+
+void update(int node, int b, int e, int i, int j, vlong x) {
+    if (i > e || j < b) {
         return;
     }
-    int Left = node * 2;
-    int Right = (node * 2) + 1;
-    int mid = (b + e) / 2;
-    update(Left, b, mid, i, j, x);
-    update(Right, mid + 1, e, i, j, x);
-    tree[node].sum = tree[Left].sum + tree[Right].sum + (e - b + 1) * tree[node].prop;
- 
-}
- 
-int query(int node, int b, int e, int i, int j, int carry = 0)
-{
-    if (i > e || j < b)
-        return 0;
- 
-    if (b >= i and e <= j)
-        return tree[node].sum + carry * (e - b + 1);
- 
-    int Left = node << 1;
-    int Right = (node << 1) + 1;
-    int mid = (b + e) >> 1;
- 
-    int p1 = query(Left, b, mid, i, j, carry + tree[node].prop);
-    int p2 = query(Right, mid + 1, e, i, j, carry + tree[node].prop);
- 
-    return p1 + p2;
-}
 
+    if (b >= i && e <= j) {
+        tr[node].sum += ((e - b + 1) * x);
+        tr[node].prop += x;
+
+        return;
+    }
+
+    int lf = node << 1, ri = lf | 1, md = b+e >> 1;
+
+    update(lf, b, md, i, j, x);
+    update(ri, md+1, e, i, j, x);
+    
+    tr[node].sum = tr[lf].sum + tr[ri].sum + (e-b+1) * tr[node].prop;
+}
+ 
+vlong query(int node, int b, int e, int i, int j, vlong carry) {
+    if (i > e || j < b) {
+        return 0;
+    }
+ 
+    if (b >= i && e <= j) {
+        vlong res = tr[node].sum + (carry * (e-b+1));
+        return res;
+    }
+
+    int lf = node << 1, ri = lf | 1, md = b+e >> 1;
+    
+    vlong x = query(lf, b, md, i, j, carry+tr[node].prop);
+    vlong y = query(ri, md+1, e, i, j, carry+tr[node].prop);
+
+    return x+y;
+}
