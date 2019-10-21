26a5b6809f8f76a86c1fb551fdc6806a2efb1d6d
Imran221b <imran221b@gmail.com>
2019-10-22 02:34:40 +0600

    Create SecondPhase.cpp

---
+++ SecondPhase.cpp
@@ -0,0 +1,36 @@
+#define Reset(N,cur) N = (N & (~(1LL << cur))) 
+#define ROF(i,x,y) for(vlong i = (y) ; i >= (x) ; --i)
+#define NMAX 2147483647
+#define LMAX 9223372036854775807LL
+
+
+inline vlong bigmod ( vlong a, vlong p, vlong m ) {
+    vlong res = 1 % m, x = a % m;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x ) % m;
+        x = ( x * x ) % m; p >>= 1;
+    }
+    return res;
+}
+
+vlong ext_gcd ( vlong A, vlong B, vlong *X, vlong *Y ){
+    vlong x2, y2, x1, y1, x, y, r2, r1, q, r;
+    x2 = 1; y2 = 0;
+    x1 = 0; y1 = 1;
+    for (r2 = A, r1 = B; r1 != 0; r2 = r1, r1 = r, x2 = x1, y2 = y1, x1 = x, y1 = y ) {
+        q = r2 / r1;
+        r = r2 % r1;
+        x = x2 - (q * x1);
+        y = y2 - (q * y1);
+    }
+    *X = x2; *Y = y2;
+    return r2;
+}
+ 
+inline vlong modInv ( vlong a, vlong m ) {
+    vlong x, y;
+    ext_gcd( a, m, &x, &y );
+    x %= m;
+    if ( x < 0 ) x += m;
+    return x;
+}
