18fa770ae34579889ae5079939703044628b4f65
Imran221b <imran221b@gmail.com>
2019-11-14 18:10:16 +0600

    Create Mobius_sgtlaugh.cpp

---
+++ Mobius_sgtlaugh.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LEN 666666
+#define MAX 10000010
+
+int len = 0, prime[LEN];
+char mu[MAX] = {0}, flag[MAX] = {0};
+
+/// Definition: Square free number is a number which doesn't have a prime factor in 
+/// its prime factorization more than once (Therefore not divisible by a square number other than 1)
+
+/// mu[1] = 1, mu[n] = 0 if n has a squared prime factor,
+/// mu[n] = 1 if n is square-free with even number of prime factors
+/// mu[n] = -1 if n is square-free with odd number of prime factors
+
+void Mobius(){ // This one is fastest of the three
+    mu[1] = 1;
+    int i, j, k;
+
+    for (i = 2; i < MAX; i++){
+        if (!flag[i]) mu[i] = -1, prime[len++] = i;
+        for (j = 0; j < len && (k = i * prime[j]) < MAX; j++){
+
+            flag[k] = true;
+            if (!(i % prime[j])){
+                mu[k] = 0;
+                break;
+            }
+            else mu[k] -= mu[i];
+        }
+    }
+}
+
+void Mobius(){ /// Simplified NlogN version
+    int i, j;
+
+    mu[1] = 1;
+    for (i = 1; i < MAX; i++){
+        for (j = i + i; j < MAX; j += i){
+            mu[j] -= mu[i];
+        }
+    }
+}
+
+void Mobius(){ /// Simplified version optimized
+    int i, j;
+
+    mu[1] = 1;
+    for (i = 1; i < MAX; i++){
+        if (mu[i]){
+            for (j = i + i; j < MAX; j += i){
+                mu[j] -= mu[i];
+            }
+        }
+    }
+}
+
+
+int main(){
+    Mobius();
+}
